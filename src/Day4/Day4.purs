f20ca3dba0579324e01fff01e4077861e7e25ce7
jtomas <me@jtomas.com>
2022-12-05 12:10:48 +0100

    day 4 part 2

--- src/Day4/Day4.purs
+++ src/Day4/Day4.purs
@@ -1,4 +1,4 @@
-module Day4 (day4part1) where
+module Day4 (day4part1, day4part2) where
 
 import Prelude
 
@@ -12,7 +12,6 @@ import Effect.Class.Console (logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
-
 day4part1 :: Effect Unit
 day4part1 = do
   readTextFile UTF8 "input-day-4" <#> split (Pattern "\n")
@@ -22,8 +21,8 @@ day4part1 = do
              >>> catMaybes
              >>> f
              )
-      >>> sum 
-      >>> logShow
+        >>> sum 
+        >>> logShow
   where
   f [a, b, x, y] = if Set.subset s1 s2 || Set.subset s2 s1  then 1 else 0
     where
@@ -31,3 +30,20 @@ day4part1 = do
     s2 = Set.fromFoldable (range x y)
   f _ = 0
 
+day4part2 :: Effect Unit
+day4part2 = do
+  readTextFile UTF8 "input-day-4" <#> split (Pattern "\n")
+    >>= map (split (Pattern ",")
+             >>> map (split (Pattern "-"))
+             >>> concat >>> map fromString
+             >>> catMaybes
+             >>> f
+             )
+        >>> sum 
+        >>> logShow
+  where
+  f [a, b, x, y] = if Set.isEmpty (Set.intersection s1 s2) then 0 else 1
+    where
+    s1 = Set.fromFoldable (range a b) 
+    s2 = Set.fromFoldable (range x y)
+  f _ = 0
--- src/Main.purs
+++ src/Main.purs
@@ -5,7 +5,7 @@ import Prelude
 import Day1 (day1part1, day1part2)
 import Day2 (day2part1, day2part2)
 import Day3 (day3part1, day3part2)
-import Day4 (day4part1)
+import Day4 (day4part1, day4part2)
 import Effect (Effect)
 
 main :: Effect Unit
@@ -16,4 +16,5 @@ main =
   -- *> day2part2  
   -- *> day3part1
   -- *> day3part2
-  day4part1
+  -- *> day4part1
+  day4part2
