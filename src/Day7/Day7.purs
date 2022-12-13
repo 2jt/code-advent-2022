b0490cdf95b915652d8590142ce600336e46c9a3
jtomas <me@jtomas.com>
2022-12-13 09:50:34 +0100

    refactor day 7

--- src/Day7/Day7.purs
+++ src/Day7/Day7.purs
@@ -8,29 +8,33 @@ import Data.Int (fromString)
 import Data.List (List(..), drop, filter, head, (:))
 import Data.Maybe (fromMaybe)
 import Data.String (Pattern(..), split)
+import Data.Traversable (sum)
 import Data.Tuple.Nested (type (/\), (/\))
 import Effect (Effect)
 import Effect.Class.Console (logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
-parseLine :: Int /\ Int /\ List Int -> Array String -> Int /\ Int /\ List Int
+parseLine :: Int /\ List Int /\ List Int -> Array String -> Int /\ List Int /\ List Int
 parseLine a@(sum /\ ans /\ list) = case _ of
-  [ "$", "cd", ".." ] -> a
-  [ "$", "cd", _ ] -> 0 /\ (if sum <= 100000 then (sum + ans) else ans) /\ sum : list
-  [ "dir", _ ] -> (sum + (fromMaybe 0 $ head list)) /\ ans /\ drop 1 list
-  [ "$", "ls" ] -> a
-  [ size, _ ] -> (sum + (fromMaybe 0 $ fromString size)) /\ ans /\ list
-  _ -> a
-
-parseLine2 :: Int /\ List Int /\ List Int -> Array String -> Int /\ List Int /\ List Int
-parseLine2 a@(sum /\ ans /\ list) = case _ of
   [ "$", "cd", ".." ] -> a
   [ "$", "cd", _ ] -> 0 /\ (sum : ans) /\ sum : list
   [ "dir", _ ] -> (sum + (fromMaybe 0 $ head list)) /\ ans /\ drop 1 list
   [ "$", "ls" ] -> a
   [ size, _ ] -> (sum + (fromMaybe 0 $ fromString size)) /\ ans /\ list
   _ -> a
+
+day7part1 :: Effect Unit
+day7part1 = solve $ foldl parseLine (0 /\ Nil /\ Nil) >>> calc >>> logShow
+  where 
+  calc (_ /\ list /\ _) = sum $ filter (_ <= 100000) list
+
+day7part2 :: Effect Unit
+day7part2 = solve $ foldl parseLine (0 /\ Nil /\ Nil) >>> calc >>> logShow
+  where
+  calc (_ /\ list /\ (total : Nil)) =
+    fromMaybe 0 $ minimum $ filter (_ >= (30000000 - (70000000 - total))) list
+  calc _ = 0
 
 solve :: (Array (Array String) -> Effect Unit) -> Effect Unit
 solve f =
@@ -39,13 +43,3 @@ solve f =
       >>> map (split (Pattern " "))
       >>> reverse
     >>= f
-
-day7part1 :: Effect Unit
-day7part1 = solve $ foldl parseLine (0 /\ 0 /\ Nil) >>> logShow
-
-day7part2 :: Effect Unit
-day7part2 = solve $ foldl parseLine2 (0 /\ Nil /\ Nil) >>> calc >>> logShow
-  where
-  calc (_ /\ list /\ (total : Nil)) =
-    fromMaybe 0 $ minimum $ filter (_ >= (30000000 - (70000000 - total))) list
-  calc _ = 0
